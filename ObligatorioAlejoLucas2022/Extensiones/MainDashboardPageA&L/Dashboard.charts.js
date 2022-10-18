3d50312b4ad92a5289768eed7adaa5b7a79dfe14
Alejo <apellejero@unlockcommerce.co>
2022-10-18 09:10:38 -0300

    table grafic: se sigue creando nuevos templates

--- ObligatorioAlejoLucas2022/Extensiones/MainDashboardPageA&L/Dashboard.charts.js
+++ ObligatorioAlejoLucas2022/Extensiones/MainDashboardPageA&L/Dashboard.charts.js
@@ -1,47 +1,30 @@
 window.addEventListener("load", onWindowLoad);
 
 function onWindowLoad() {
-  const labels = Utils.months({ count: 7 });
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: "My First Dataset",
-        data: [65, 59, 80, 81, 56, 55, 40],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-          "rgba(255, 205, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(201, 203, 207, 0.2)",
-        ],
-        borderColor: [
-          "rgb(255, 99, 132)",
-          "rgb(255, 159, 64)",
-          "rgb(255, 205, 86)",
-          "rgb(75, 192, 192)",
-          "rgb(54, 162, 235)",
-          "rgb(153, 102, 255)",
-          "rgb(201, 203, 207)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+  var xValues = ["Canceladas", "Aprobadas", "Ingoradas", "Pendientes"];
+  var yValues = [55, 15, 44, 24];
+  var barColors = [
+    "#b91d47",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
+  ];
 
-  const config = {
-    type: "bar",
-    data: data,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
+  new Chart("myChart", {
+    type: "doughnut",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
     },
-  };
-
-  return new Chart(document.querySelector("myChart-2"), config);
+    options: {
+      title: {
+        display: true,
+        text: "World Wide Wine Production 2018"
+      }
+    }
+  });
 }
