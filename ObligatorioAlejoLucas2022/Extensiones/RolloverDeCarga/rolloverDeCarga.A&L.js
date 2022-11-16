2c65fc879a759690c5e973208cb01e536b0c6c60
Alejo <apellejero@unlockcommerce.co>
2022-11-15 22:57:23 -0300

    Aun faltan resolver varias cosas

--- ObligatorioAlejoLucas2022/Extensiones/RolloverDeCarga/rolloverDeCarga.A&L.js
+++ ObligatorioAlejoLucas2022/Extensiones/RolloverDeCarga/rolloverDeCarga.A&L.js
@@ -1,3 +1,30 @@
 function onRolloverDeCarga() {
+    const TablaToRollover = OBJ1Selector.FatherDivTable;
+    const BodyTableRollover = getQuerySelector(".", "tabla-de-viajes-rollover-de-carga", true);
+    const ViajeId = parseInt(this.getAttribute("data-id"));
+    const TablaViajesCargaAttr = getQuerySelector('.', 'fila-show-carga-peligrosa', false);
+    const FatherDivTable = OBJ1Selector.TablaRollover;
+
+    TablaViajesCargaAttr.forEach(function (fila) {
+        let filaId = parseInt(fila.getAttribute("data-id"));
+        if (filaId === ViajeId) {
+            setDisplay(fila, false);
+        }
+    });
+
+    //Muestro todos los viajes existentes menos el que quiero hacer rollover
+    Viajes.forEach(function (viaje, index) {
+        if (ViajeId !== viaje.id) {
+            createTableViajes(BodyTableRollover, viaje, index, 'rollover-to-this-fila-attr');
+        }
+    });
+
+    addAFunctionToAFila(getQuerySelector('.', "rollover-to-this-fila-attr", false), addToThisViaje)
+
+    setDisplay(FatherDivTable, true);
+    setDisplay(TablaToRollover, true);
+}
+
+function addToThisViaje() {
     debugger;
 }
